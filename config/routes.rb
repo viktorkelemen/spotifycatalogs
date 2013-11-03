87ae02f1bdf668c4007552d419ab7dc87c36ad73
Viktor Kelemen <vkelemen@yammer-inc.com>
2013-11-03 01:20:28 +0000

    Change root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SpoturaWeb::Application.routes.draw do
 
   resources :albums
 
-  root 'home#index'
+  root 'albums#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
